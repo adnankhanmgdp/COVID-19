3a4af9ef1c1224b20e8b180460d22434bc153689
adnan khan <adnankhanmgdp@gmail.com>
2020-03-23 13:58:33 +0530

    Update
    Update

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+import pickle
+
+app = Flask(__name__)
+
+file = open('model.pkl', 'rb')
+clf= pickle.load(file)
+file.close()
+@app.route('/', methods=["GET", "POST"])
+
+def hello_world():
+    if request.method == "POST":
+        myDict = request.form
+        fever=float(myDict['fever'])
+        age=int(myDict['Age'])
+        pain=int(myDict['pain'])
+        Nose=int(myDict['Nose'])
+        breathing=int(myDict['breathing'])
+        inputFeatures=[fever,age,pain,Nose,breathing]
+        infProb=clf.predict_proba([inputFeatures])[0][1]
+        print(infProb)
+        return render_template('show.html',inf=round(infProb*100))
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
